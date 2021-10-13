25c3e7dd034cae93b3d7b011b4e0f353d7fbeaac
hi20160616 <hi20160616@163.com>
2021-10-13 13:25:35 +0800

    first commit

---
+++ internal/fetcher/article.go
@@ -0,0 +1,262 @@
+package fetcher
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"log"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/hi20160616/exhtml"
+	"github.com/hi20160616/gears"
+	"github.com/hi20160616/ms-appledaily/configs"
+	"github.com/pkg/errors"
+	"golang.org/x/net/html"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type Article struct {
+	Id            string
+	Title         string
+	Content       string
+	WebsiteId     string
+	WebsiteDomain string
+	WebsiteTitle  string
+	UpdateTime    *timestamppb.Timestamp
+	U             *url.URL
+	raw           []byte
+	doc           *html.Node
+}
+
+var ErrTimeOverDays error = errors.New("article update time out of range")
+
+func NewArticle() *Article {
+	return &Article{
+		WebsiteDomain: configs.Data.MS["appledaily"].Domain,
+		WebsiteTitle:  configs.Data.MS["appledaily"].Title,
+		WebsiteId:     fmt.Sprintf("%x", md5.Sum([]byte(configs.Data.MS["appledaily"].Domain))),
+	}
+}
+
+// List get all articles from database
+func (a *Article) List() ([]*Article, error) {
+	return load()
+}
+
+// Get read database and return the data by rawurl.
+func (a *Article) Get(id string) (*Article, error) {
+	as, err := load()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range as {
+		if a.Id == id {
+			return a, nil
+		}
+	}
+	return nil, fmt.Errorf("[%s] no article with id: %s",
+		configs.Data.MS["appledaily"].Title, id)
+}
+
+func (a *Article) Search(keyword ...string) ([]*Article, error) {
+	as, err := load()
+	if err != nil {
+		return nil, err
+	}
+
+	as2 := []*Article{}
+	for _, a := range as {
+		for _, v := range keyword {
+			v = strings.ToLower(strings.TrimSpace(v))
+			switch {
+			case a.Id == v:
+				as2 = append(as2, a)
+			case a.WebsiteId == v:
+				as2 = append(as2, a)
+			case strings.Contains(strings.ToLower(a.Title), v):
+				as2 = append(as2, a)
+			case strings.Contains(strings.ToLower(a.Content), v):
+				as2 = append(as2, a)
+			case strings.Contains(strings.ToLower(a.WebsiteDomain), v):
+				as2 = append(as2, a)
+			case strings.Contains(strings.ToLower(a.WebsiteTitle), v):
+				as2 = append(as2, a)
+			}
+		}
+	}
+	return as2, nil
+}
+
+type ByUpdateTime []*Article
+
+func (u ByUpdateTime) Len() int      { return len(u) }
+func (u ByUpdateTime) Swap(i, j int) { u[i], u[j] = u[j], u[i] }
+func (u ByUpdateTime) Less(i, j int) bool {
+	return u[i].UpdateTime.AsTime().Before(u[j].UpdateTime.AsTime())
+}
+
+var timeout = func() time.Duration {
+	t, err := time.ParseDuration(configs.Data.MS["appledaily"].Timeout)
+	if err != nil {
+		log.Printf("[%s] timeout init error: %v", configs.Data.MS["appledaily"].Title, err)
+		return time.Duration(1 * time.Minute)
+	}
+	return t
+}()
+
+// fetchArticle fetch article by rawurl
+func (a *Article) fetchArticle(rawurl string) (*Article, error) {
+	var err error
+	a.U, err = url.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	// Dail
+	a.raw, a.doc, err = exhtml.GetRawAndDoc(a.U, 1*time.Minute)
+	if err != nil {
+		if strings.Contains(err.Error(), "invalid header") {
+			a.Title = a.U.Path
+			a.UpdateTime = timestamppb.Now()
+			a.Content, err = a.fmtContent("")
+			if err != nil {
+				return nil, err
+			}
+			return a, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	a.Id = fmt.Sprintf("%x", md5.Sum([]byte(rawurl)))
+
+	a.Title, err = a.fetchTitle()
+	if err != nil {
+		return nil, err
+	}
+
+	a.UpdateTime, err = a.fetchUpdateTime()
+	if err != nil {
+		return nil, err
+	}
+
+	// content should be the last step to fetch
+	a.Content, err = a.fetchContent()
+	if err != nil {
+		return nil, err
+	}
+
+	a.Content, err = a.fmtContent(a.Content)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+
+}
+
+func (a *Article) fetchTitle() (string, error) {
+	n := exhtml.ElementsByTag(a.doc, "title")
+	if n == nil {
+		return "", fmt.Errorf("[%s] getTitle error, there is no element <title>",
+			configs.Data.MS["appledaily"].Title)
+	}
+	title := n[0].FirstChild.Data
+	rp := strings.NewReplacer(" - 纽约时报中文网", "")
+	title = strings.TrimSpace(rp.Replace(title))
+	return gears.ChangeIllegalChar(title), nil
+}
+
+func (a *Article) fetchUpdateTime() (*timestamppb.Timestamp, error) {
+	if a.raw == nil {
+		return nil, errors.Errorf("[%s] fetchUpdateTime: raw is nil: %s",
+			configs.Data.MS["appledaily"].Title, a.U.String())
+	}
+
+	t := time.Now() // if no time fetched, return current time
+	var err error
+	n := exhtml.MetasByName(a.doc, "date")
+	if len(n) == 0 {
+		return nil, fmt.Errorf("[%s] fetchUpdateTime error, no meta named date matched: %s",
+			configs.Data.MS["appledaily"].Title, a.U.String())
+	}
+	for _, nn := range n {
+		for _, x := range nn.Attr {
+			if x.Key == "content" {
+				t, err = time.Parse(time.RFC3339, x.Val)
+				if err != nil {
+					return nil, errors.WithMessage(err,
+						"caught meta but no content matched.")
+				}
+			}
+		}
+	}
+
+	if t.Before(time.Now().AddDate(0, 0, -3)) {
+		return timestamppb.New(t), ErrTimeOverDays
+	}
+	return timestamppb.New(t), err
+}
+
+func shanghai(t time.Time) time.Time {
+	loc := time.FixedZone("UTC", 8*60*60)
+	return t.In(loc)
+}
+
+func (a *Article) fetchContent() (string, error) {
+	if a.doc == nil {
+		return "", errors.Errorf("[%s] fetchContent: doc is nil: %s", configs.Data.MS["appledaily"].Title, a.U.String())
+	}
+	body := ""
+	bodyN := exhtml.ElementsByTagAndClass(a.doc, "section", "article-body")
+	if len(bodyN) == 0 {
+		return body, errors.Errorf("no article content matched: %s", a.U.String())
+	}
+	// Fetch content
+	ps := exhtml.ElementsByTagAndClass(bodyN[0], "div", "article-paragraph")
+	for _, v := range ps {
+		var buf bytes.Buffer
+		if v.FirstChild == nil {
+			continue
+		}
+		// skip figure paragraph
+		if v.FirstChild.Type == html.ElementNode && v.FirstChild.Data == "figure" {
+			continue
+		}
+		if err := html.Render(&buf, v); err != nil {
+			err = fmt.Errorf("render node to bytes error: %s", a.U.String())
+			continue
+		}
+		re := regexp.MustCompile(`(?m)<div.*?>(?P<paragraph>.*?)</div>`)
+		x := re.ReplaceAllString(buf.String(), "${paragraph}  \n")
+
+		re = regexp.MustCompile(`(?m)<span>(?P<span>.*?)</span>`)
+		x = re.ReplaceAllString(x, "${span}")
+
+		re = regexp.MustCompile(`(?m)<a.*?href="(?P<href>.*)".*?>(?P<title>.*?)</a>`)
+		x = re.ReplaceAllString(x, "[${title}](${href})")
+
+		body += x
+	}
+	return body, nil
+}
+
+func (a *Article) fmtContent(body string) (string, error) {
+	var err error
+	title := "# " + a.Title + "\n\n"
+	lastupdate := shanghai(a.UpdateTime.AsTime()).Format(time.RFC3339)
+	webTitle := fmt.Sprintf(" @ [%s](/list/?v=%[1]s): [%[2]s](http://%[2]s)", a.WebsiteTitle, a.WebsiteDomain)
+	u, err := url.QueryUnescape(a.U.String())
+	if err != nil {
+		u = a.U.String() + "\n\nunescape url error:\n" + err.Error()
+	}
+	body = title +
+		"LastUpdate: " + lastupdate +
+		webTitle + "\n\n" +
+		"---\n" +
+		body + "\n\n" +
+		"原地址：" + fmt.Sprintf("[%s](%[1]s)", strings.Split(u, "?tmpl=")[0])
+	return body, nil
+}
---
+++ internal/fetcher/article_test.go
@@ -0,0 +1,132 @@
+package fetcher
+
+import (
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/hi20160616/exhtml"
+	"github.com/pkg/errors"
+)
+
+func TestFetchTitle(t *testing.T) {
+	tests := []struct {
+		url   string
+		title string
+	}{
+		{"https://cn.appledaily.com/china/20211011/china-child-custody-abductions/?utm_source=RSS", "监护权争夺战：那些绑架自己孩子的中国家长"},
+		{"https://cn.appledaily.com/asia-pacific/20201201/north-korea-pandemic-china/", "疫情进一步冲击对华贸易，朝鲜经济困境加深"},
+	}
+	for _, tc := range tests {
+		a := NewArticle()
+		u, err := url.Parse(tc.url)
+		if err != nil {
+			t.Error(err)
+		}
+		a.U = u
+		// Dail
+		a.raw, a.doc, err = exhtml.GetRawAndDoc(a.U, timeout)
+		if err != nil {
+			t.Error(err)
+		}
+		got, err := a.fetchTitle()
+		if err != nil {
+			if !errors.Is(err, ErrTimeOverDays) {
+				t.Error(err)
+			} else {
+				fmt.Println("ignore pass test: ", tc.url)
+			}
+		} else {
+			if tc.title != got {
+				t.Errorf("\nwant: %s\n got: %s", tc.title, got)
+			}
+		}
+	}
+
+}
+
+func TestFetchUpdateTime(t *testing.T) {
+	tests := []struct {
+		url  string
+		want string
+	}{
+		{"https://cn.appledaily.com/china/20211011/china-child-custody-abductions/?utm_source=RSS", "2021-10-11 17:41:03 +0800 UTC"},
+		{"https://cn.appledaily.com/asia-pacific/20201201/north-korea-pandemic-china/", "2020-12-01 19:04:20 +0800 UTC"},
+	}
+	var err error
+	for _, tc := range tests {
+		a := NewArticle()
+		a.U, err = url.Parse(tc.url)
+		if err != nil {
+			t.Error(err)
+		}
+		// Dail
+		a.raw, a.doc, err = exhtml.GetRawAndDoc(a.U, timeout)
+		if err != nil {
+			t.Error(err)
+		}
+		tt, err := a.fetchUpdateTime()
+		if err != nil {
+			if !errors.Is(err, ErrTimeOverDays) {
+				t.Error(err)
+			}
+		}
+		ttt := tt.AsTime()
+		got := shanghai(ttt)
+		if got.String() != tc.want {
+			t.Errorf("\nwant: %s\n got: %s", tc.want, got.String())
+		}
+	}
+}
+
+func TestFetchContent(t *testing.T) {
+	tests := []struct {
+		url  string
+		want string
+	}{
+		{"https://cn.appledaily.com/china/20211011/china-child-custody-abductions/?utm_source=RSS", "监护权争夺战：那些绑架自己孩子的中国家长"},
+		// {"https://cn.appledaily.com/asia-pacific/20201201/north-korea-pandemic-china/", "疫情进一步冲击对华贸易，朝鲜经济困境加深"},
+	}
+	var err error
+
+	for _, tc := range tests {
+		a := NewArticle()
+		a.U, err = url.Parse(tc.url)
+		if err != nil {
+			t.Error(err)
+		}
+		// Dail
+		a.raw, a.doc, err = exhtml.GetRawAndDoc(a.U, timeout)
+		if err != nil {
+			t.Error(err)
+		}
+		c, err := a.fetchContent()
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(c)
+	}
+}
+
+func TestFetchArticle(t *testing.T) {
+	tests := []struct {
+		url string
+		err error
+	}{
+		{"https://cn.appledaily.com/china/20211011/china-child-custody-abductions/?utm_source=RSS", ErrTimeOverDays},
+		{"https://cn.appledaily.com/asia-pacific/20201201/north-korea-pandemic-china/", nil},
+	}
+	for _, tc := range tests {
+		a := NewArticle()
+		a, err := a.fetchArticle(tc.url)
+		if err != nil {
+			if !errors.Is(err, ErrTimeOverDays) {
+				t.Error(err)
+			} else {
+				fmt.Println("ignore old news pass test: ", tc.url)
+			}
+		} else {
+			fmt.Println("pass test: ", a.Content)
+		}
+	}
+}
